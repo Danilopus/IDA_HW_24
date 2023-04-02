d7e498fc4821bfac9361b6054802c8a249f2336e
Ишутинов Данил <gladius@inbox.ru>
2023-04-02 22:18:32 +0500

    Add project files.

---
+++ IDA_HW_24/IDA_Home_24.cpp
@@ -0,0 +1,322 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+//#include <array>
+
+// Service functions prototypes ------------------------------------
+int Get_Int_SA();			// String-analysis вариант фильтра INTEGER
+int Get_Int_Positive();		// String-Analysis вариант проверки ввода INTEGER_POSITIVE		
+int Get_Int_PG();			// peek&get вариант проверки ввода INTEGER	
+double Get_Dbl_SA();		// String-analysis вариант фильтра DOUBLE.
+double Get_Dbl_Positive();	// String-analysis вариант фильтра DOUBLE_POSITIVE
+double Get_Dbl_PG();		// peek&get вариант проверки ввода DOUBLE	
+int Get_Random(int Range_min, int Range_max);
+double Get_Random(double Range_min, double Range_max);
+template <typename ANY> void Fill_Array_with_Random(ANY arr[], int Size, ANY Range_min, ANY Range_max);
+template <typename ANY> void Array_Console_Out(ANY arr[], int Size);
+
+// Homework 24 ----------------------------------------------------	
+
+//Task 1
+void Array_consolidation()
+{
+	int n = 10, m = 5;
+	double Random_Range = 100;
+	double* Dynamic_double_array_A = new double[n];
+	double* Dynamic_double_array_B = new double[m];
+	double* Dynamic_double_array_C = new double[m+n];
+	Fill_Array_with_Random(Dynamic_double_array_A, n, -Random_Range, Random_Range);
+	Fill_Array_with_Random(Dynamic_double_array_B, m, -Random_Range, Random_Range);
+	std::cout << "Array A:";
+	Array_Console_Out(Dynamic_double_array_A, n);
+	std::cout << "Array B:";
+	Array_Console_Out(Dynamic_double_array_B, m);
+	for (int i = 0; i < n; i++)
+		Dynamic_double_array_C[i] = Dynamic_double_array_A[i];
+	for (int i = 0; i < m; i++)
+		Dynamic_double_array_C[i+n] = Dynamic_double_array_B[i];
+	std::cout << "\nArray C:";
+	Array_Console_Out(Dynamic_double_array_C, m+n);
+	delete[] Dynamic_double_array_A;
+	delete[] Dynamic_double_array_B;
+}
+//Task 2
+template <typename any> void resize_arr(any*& Original_array, int Size, int newSize)
+{
+	any* New_dynamic_array = new any[newSize];
+	for (int i = 0; i < newSize; i++)
+	{
+		if (i >= Size) New_dynamic_array[i] = i;
+		else New_dynamic_array[i] = Original_array[i];
+	}
+		delete[] Original_array;
+		Original_array = New_dynamic_array;
+}
+void resize_arr()
+{
+	int Size = 10, newSize = 20;
+	float* Dynamic_float_array = new float[Size];
+	Fill_Array_with_Random(Dynamic_float_array, Size, -1.0f, 1.0f);
+	std::cout << "Original array [10]:";
+	Array_Console_Out(Dynamic_float_array, Size);	
+	resize_arr(Dynamic_float_array, Size, newSize);
+	std::cout << "\nResized array [20]:";
+	Array_Console_Out(Dynamic_float_array, newSize);
+}
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	//system("mode con cols=110 lines=40"); 
+	std::string Name_of_Work = { "Home Work 24: Dynamic arrays" };
+	std::string Menu_Element_1 = { "Array C[n+m] from arrays A[n] & B[m]" };
+	std::string Menu_Element_2 = { "resize_arr" };
+	//std::string Menu_Element_3 = { "XXX" };
+	//std::string Menu_Element_4 = { "XXX" };
+
+	do {		
+		system("cls");
+		std::cout << "\n\t***\t" << Name_of_Work << "\t***\n\n\t\n\nChoose an option: \n";
+		std::cout << "\n 1. " << Menu_Element_1;
+		std::cout << "\n 2. " << Menu_Element_2;
+		//std::cout << "\n 3. " << Menu_Element_3;
+		//std::cout << "\n 4. " << Menu_Element_4;
+		std::cout << "\n\n 0. Exit\n";
+		int choice = 0;
+		std::cout << "\nYour choice: ";
+		choice = Get_Int_Positive();		
+		std::cout << "\n";
+		// Обработка выбора пользователя
+		if (choice == 0) { std::cout << "\nGood By"; for (int j = 0; j < 50; j++) { Sleep(50 - j); std::cout << "y"; } std::cout << "e!"; Sleep(850);  return 0; }
+		else if (choice == 1) Array_consolidation();
+		else if (choice == 2) resize_arr();
+		//else if (choice == 3) Sort_Array_from_N_demo();
+		//else if (choice == 4) Search_First_Ordinary_in_Array_demo();
+			
+		else { std::cout << "\nSuch choice does not exist yet\n"; Sleep(1000); }
+		std::cout << "\n\n";
+		system("pause");
+		} while (true);
+		
+	//system("pause");
+	return 0;
+}
+
+// Service functions -----------------------------------------
+int Get_Int_SA() {
+	std::string a;
+	std::cin.sync(); 
+	std::cin.clear(); 
+	getline(std::cin, a);
+	if (a != "")
+	{
+
+		if ((a.find_first_not_of("-0123456789") == -1))
+		{
+			for (int i = 1; i < a.size(); i++) // определяем есть ли минусы кроме первого символа
+			{
+				if (a[i] == '-') 
+				{
+					std::cout << "Input Error: Minus misstanding. [INTEGER] expected.\n";
+					return Get_Int_SA();
+				}
+			}
+			try
+			{
+				return stoi(a);
+			}
+			catch (std::out_of_range)
+			{
+				std::cout << "Input Error: Data type overflow. [INTEGER] expected.\n";
+				return Get_Int_SA();
+			}
+
+
+		}
+		else
+		{
+			std::cout << "Input Error: Incorrect symbol. [INTEGER] expected.\n";
+			return Get_Int_SA();
+		}
+	}
+	else
+	{
+		std::cout << "Input Error: NULL input. [INTEGER] expected.\n";
+		return Get_Int_SA();
+	}
+}
+int Get_Int_Positive()
+{
+	std::string a;
+	std::cin.sync();
+	std::cin.clear();
+	getline(std::cin, a);
+	if (a != "") 
+	{
+
+		if ((a.find_first_not_of("0123456789") == -1))
+		{
+			try{
+				return stoi(a);
+			}
+			catch (std::out_of_range){
+				std::cout << "Input Error: overflow. Positive [INTEGER] expected.\n";
+			}
+		}
+		else
+		{
+			std::cout << "Input Error: Positive [INTEGER] expected.\n";
+			return Get_Int_Positive();
+		}		
+	}
+	else
+		std::cout << "Input Error: NULL input. Positive [INTEGER] expected.\n";
+	return Get_Int_Positive();
+}
+int Get_Int_PG()
+{
+	int a;
+	while (!(std::cin >> a) || (std::cin.peek() != '\n'))
+	{
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Input error! [INTEGER] expected. Repeat please...\n";
+	}
+	return a;
+}
+double Get_Dbl_SA(){
+	std::string a;
+	std::cin.sync();
+	getline(std::cin, a);
+	if (a == "") { std::cout << "\nInput Error : NULL input\n"; return Get_Dbl_SA(); }
+	if ((a.find_first_not_of("-.0123456789") == -1))
+	{
+		for (int i = 1; i < a.size(); i++) // Проверяем отсутсвие лишних минусов
+		{
+			if (a[i] == '-')
+			{
+				std::cout << "\nInput Error: Minus misstanding. [DOUBLE] expected.\n";
+				return Get_Dbl_SA();
+			}
+
+		}
+		bool Dot_Flag = 0;
+		for (int i = 0; i < a.size(); i++) // проверяем отсутсвие лишних точек
+		{
+			if (a[i] == '.')
+			{
+				if (Dot_Flag == 1)
+				{
+					std::cout << "\nInput Error: Dot misstandig. [DOUBLE] expected.\n";
+					return Get_Dbl_SA();
+				}
+				Dot_Flag = 1;
+			}
+
+		}		 	
+				try{
+					return stod(a);
+				}
+				catch (std::out_of_range) {
+					std::cout << "Input Error: overflow. [DOUBLE] expected.\n";
+					return Get_Dbl_SA();					
+				}						
+	}
+	else {		
+		std::cout << "Incorrect symbols: [DOUBLE] expected.\nUse dot instead of coma. Example 50.64\n";
+		return Get_Dbl_SA();
+	}
+}
+double Get_Dbl_Positive(){
+	std::string a, dot = ".";
+	std::cin.sync();
+	std::cin.clear();
+	getline(std::cin, a);
+	if (a != "")
+	{
+		if ((a.find_first_not_of(".0123456789") == -1))
+		{
+			bool One_Dot_already = false;
+			for (int i = 0; i < a.size(); i++)
+			{				
+				if (a[i] == dot[0] && One_Dot_already == true) //проверяем количество разделителей
+				{
+					std::cout << "Input Error: Dot misstandig. Positive [DOUBLE] expected.\n";
+					return Get_Dbl_Positive();
+				}
+				if (a[i] == dot[0])	One_Dot_already = true;
+			}
+			try
+			{				
+				return stod(a);
+			}
+			catch (std::out_of_range)
+			{
+				std::cout << "Input Error: Data type overflow. Positive [DOUBLE] expected.\n";
+				return Get_Dbl_Positive();
+			}
+		}
+		else
+		{
+			//std::cout << "Incorrect symbols: positive REAL expected.\nUse coma instead of dot. Example 50,64\n"; // да точно говорю вот даже сообщение такое было, а теперь наоборот wtf*2?!
+			std::cout << "Incorrect symbols: Positive [DOUBLE] expected.\nUse dot instead of coma. Example 50.64\n";
+			return Get_Dbl_Positive();
+		}
+	}
+	else
+	{
+		std::cout << "Input Error: NULL input. Positive [DOUBLE] expected.\n";
+		return Get_Dbl_Positive();
+	}
+	return Get_Dbl_Positive();
+}
+double Get_Dbl_PG()
+{
+	double a;
+	while (!(std::cin >> a) || (std::cin.peek() != '\n'))
+	{
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Input error! [DOUBLE] expected. Repeat please...\n";
+	}
+	return a;
+}
+int Get_Random(int Range_min=-999, int Range_max=999)
+{
+	if (Range_min > Range_max) 
+	{ 
+		std::cout << "\nError: Range_min > Range_max. New range: [Range_max..Range_min]\n";
+		std::swap(Range_min, Range_max);
+	}
+	//return (Range_min + rand() % (Range_max - Range_min + 1));
+	double K_random = double((rand() % RAND_MAX)) / RAND_MAX;
+	int random = Range_min + ((Range_max - Range_min) * K_random);
+	//return Range_min + (Range_max - Range_min) * (rand() % RAND_MAX) / RAND_MAX;
+	return random;
+}
+double Get_Random(double Range_min=-9999.0, double Range_max=9999.0)
+{
+	if (Range_min > Range_max)
+	{
+		std::cout << "\nError: Range_min > Range_max. New range: [Range_max..Range_min]\n";
+		std::swap(Range_min, Range_max);
+	}
+	return 	Range_min + (Range_max - Range_min) * double(rand() % RAND_MAX) / RAND_MAX;	
+}
+template <typename ANY> void Array_Console_Out(ANY arr[], int Size)
+{
+	std::cout << "\n[";
+	for (int i = 0; i < Size; i++)
+		std::cout << arr[i] << ", ";
+	std::cout << "\b\b]\n";
+}
+template <typename ANY> void Fill_Array_with_Random(ANY arr[], int Size, ANY Range_min, ANY Range_max)
+{
+	for (int i = 0; i < Size; i++)
+	{
+		arr[i] = Get_Random(Range_min, Range_max);		
+	}
+}
